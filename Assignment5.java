1aa1137caada60068e66537bc41ac48d73954e5c
cuifangyu <38642590+cuifangyu@users.noreply.github.com>
2019-10-11 14:08:12 -0700

    Add files via upload

---
+++ Assignment5.java
@@ -0,0 +1,18 @@
+package Assignment5;
+
+public class Assignment5 {
+
+    //Explain the difference between `final` and `finally` in Java 8.
+
+    /*
+    "final" is used in class/method/variable to make it unchangeable.
+    If a class is final, it can not be inherited by another Class.
+    If a method is final, it can not be override in subclass.
+    If a variable is final, it can not be assigned to another value.
+
+    "finally" is generally used when dealing with exceptions.
+    We can use try...catch...finally... to deal with exceptions.
+    The statement in "finally" will always be executed.
+     */
+
+}
