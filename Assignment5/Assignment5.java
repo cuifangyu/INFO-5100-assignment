73d0bdc4b424311ed7f0f07b3e8a48e5eb69ca4b
cuifangyu <38642590+cuifangyu@users.noreply.github.com>
2019-10-12 21:56:41 -0700

    Add files via upload

--- Assignment5/Assignment5.java
+++ Assignment5/Assignment5.java
@@ -15,4 +15,25 @@ public class Assignment5 {
     The statement in "finally" will always be executed.
      */
 
+
+    //Extra Credits
+
+    /*
+    The answer is 6. The result of getAddCount is 6 and the result of getI is 0.
+
+    When b use addAll method, as the method is override, it will call the override method in B class.
+    So first addCount will be 0 + 3 = 3;
+    Then it uses super to call the method in A class. In the addCount method in A class, it calls the add method.
+    As B class override the add method, it will call the method in B class instead of A.
+    When each element is added into the list, addCount will add 1.
+    So after the whole process, the addCount number will be 6.
+
+    When b use getI method, as it is not override in B, it will call the method in A class.
+    When a method calls a member variable, the variable it chooses depends on the class of method being defined.
+    So the method of getI will use the I in A class, which is 0.
+    So the result of getI is 0.
+
+    So, the sum of addCount and getI is 6.
+     */
+
 }
